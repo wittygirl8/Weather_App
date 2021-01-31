0571890bca81cde30a63fea2830197249e00493d
Riddhi <riddhisinghreena238@gmail.com>
2021-01-31 16:16:44 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
-const fs=require('fs');
+const fs = require('fs');
 var requests = require("requests");
 const homeFile = fs.readFileSync('home.html', 'utf-8');
-const replaceVal= (tempVal, orgVal) => {
+const replaceVal = (tempVal, orgVal) => {
     let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
     temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
     temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
@@ -12,29 +12,26 @@ const replaceVal= (tempVal, orgVal) => {
     return temperature;
 }
 
-const inp;
-function oncl(){
-    const inp=document.getElementsByClassName(inputValue);
-}
-const server =http.createServer((req,res) => {
-    if(req.url == "/"){
-        requests('http://api.openweathermap.org/data/2.5/weather?q=Chennai&units=metric&appid=5e549ce893713ddae68c0a4dfb06cb07'
-         )
-        .on('data', (chunk) => {
-            const objdata = JSON.parse(chunk);
-            const arrData = [objdata];
-            //console.log(arrData[0].main.temp);
-            const realTimeData = arrData.map((val) => replaceVal(homeFile, val)).join("");
-            res.write(realTimeData);
 
-            //console.log(realTimeData);
-         })
-        .on('end', (err) => {
-            if (err) return console.log('connection closed due to errors', err);
-            res.end();
-            console.log('end');
-        });
-        
+const server = http.createServer((req, res) => {
+    if (req.url == "/") {
+        requests('http://api.openweathermap.org/data/2.5/weather?q=Chennai&units=metric&appid=5e549ce893713ddae68c0a4dfb06cb07'
+        )
+            .on('data', (chunk) => {
+                const objdata = JSON.parse(chunk);
+                const arrData = [objdata];
+                //console.log(arrData[0].main.temp);
+                const realTimeData = arrData.map((val) => replaceVal(homeFile, val)).join("");
+                res.write(realTimeData);
+
+                //console.log(realTimeData);
+            })
+            .on('end', (err) => {
+                if (err) return console.log('connection closed due to errors', err);
+                res.end();
+                console.log('end');
+            });
+
     }
 })
 
